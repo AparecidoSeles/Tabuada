a50b42e25f92409bb901205f53342b39e419eb45
junior <you@example.com>
2020-11-18 12:08:52 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma tabuada");
+            int tabuada = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(tabuada+ " x " +i+ " = "+tabuada*i);
+            }
+        }
+    }
+}
